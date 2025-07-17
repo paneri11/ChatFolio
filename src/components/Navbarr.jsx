850640359749cdec0e3d16446e27e269470e6e28
Paneri Fulbandhe <fulbandhe9129@gmail.com>
2025-07-17 10:14:29 +0530

    updated the Navbarr file

--- src/components/Navbarr.jsx
+++ src/components/Navbarr.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/JD.png";
+import logo from "../assets/PF.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
